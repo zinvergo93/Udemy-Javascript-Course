88008d337e0419a6b13913c90655adfda502a58f
Zachary Invergo <zinvergo@gmail.com>
2021-04-17 07:46:57 -0500

    Values and Variables tutorial + assignment completed

---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,34 @@
+let js = "amazing";
+
+// if (js === "amazing") alert("Javascript is fun!!");
+
+console.log(40 + 8 + 23 - 10);
+
+// Values and Variables //
+//////////////////////////////////
+console.log(js);
+console.log("Jonas");
+console.log(27);
+let firstName = "Zac";
+let PI = 3.1415;
+console.log(firstName);
+
+let myFirstJob = "Paper boy";
+let myLastJob = "Pepsi Service Technician";
+let myNextJob = "Junior Developer";
+
+let myJobs = [myFirstJob, myLastJob, myNextJob];
+
+console.log(myJobs);
+console.log("My last job was working as a", myJobs[1]);
+
+// ASSIGNMENT SECTION //
+// declare variables called "country", "continent" and "population" and assign their values according to your own country (population in millions) then log in the console
+
+let country = "The United States of America";
+let continent = "North America";
+let population = 320 + " million";
+
+console.log(
+  `I am from ${country}, located in the continent of ${continent}. The current population is ${population}`
+);
