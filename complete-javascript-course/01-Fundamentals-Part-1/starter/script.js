55164b0000a523884e3e89a05f651f101b33bb1b
Zachary Invergo <zinvergo@gmail.com>
2021-04-19 06:09:01 -0500

    var, let and const tutorial

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+// let js = "amazing";
 
 // if (js === "amazing") alert("Javascript is fun!!");
 
@@ -6,12 +6,14 @@ let js = "amazing";
 
 // Values and Variables //
 //////////////////////////////////
-// console.log(js);
-// console.log("Jonas");
-// console.log(27);
-// let firstName = "Zac";
+
+/*
+console.log(js);
+console.log("Jonas");
+console.log(27);
+let firstName = "Zac";
 let PI = 3.1415;
-// console.log(firstName);
+console.log(firstName);
 
 let myFirstJob = "Paper boy";
 let myLastJob = "Pepsi Service Technician";
@@ -19,8 +21,9 @@ let myNextJob = "Junior Developer";
 
 let myJobs = [myFirstJob, myLastJob, myNextJob];
 
-// console.log(myJobs);
-// console.log("My last job was working as a", myJobs[1]);
+console.log(myJobs);
+console.log("My last job was working as a", myJobs[1]);
+*/
 
 // ASSIGNMENT SECTION //
 // declare variables called "country", "continent" and "population" and assign their values according to your own country (population in millions) then log in the console
@@ -29,16 +32,20 @@ let myJobs = [myFirstJob, myLastJob, myNextJob];
   let country = "The United States of America";
   let continent = "North America";
   let population = 320 + " million";
-  */
+*/
 
 // console.log(
 //   `I am from ${country}, located in the continent of ${continent}. The current population is ${population}`
 // );
+// console.log(typeof country) >>> "string"
+// console.log(typeof population) >>> "string"
 
-// ********************************************* //
+// *********************NEW SECTION************************ //
 // Data Types //
 // Object or Primitive
 // Primitive Data type Number, String, Boolean, undefined, null, symbol, bigInt
+
+/*
 // Number // - Floating point numbers >>> Used for decimals and integers
 let age = 27;
 console.log(typeof age);
@@ -66,3 +73,39 @@ console.log(typeof null); // Returns "object", which is considered a 'bug' in Ja
 // Symbol (ES2015) // - Value that is unique and cannot be changed [Not useful for now for current lessons]
 //////////////////
 // BigInt (ES2020) // - Larger integers than the Number type can hold.
+*/
+
+// Assignment on Data Types //
+
+/*
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+// "boolean"
+console.log(typeof language);
+// "undefined"
+*/
+
+// ********************NEW SECTION*****************************//
+// var, let and const //
+// var >>> originally used to declare and define a variable, and was changable. Don't use anymore.
+// let >>> used to declare a variable that can be changed later
+// const >>> used to declare a variable that *cannot* be changed later
+
+let age = 27;
+age = 28;
+// age is now reassigned to 28
+const birthYear = 1993;
+
+// birthYear = 1995
+// Will throw error in console
+
+// const job;
+// **Missing initializer in const declaration** In other words, you cannot leave a const variable as undefined
+
+// Assignment on let, const and var //
+
+let language = "English";
+const gender = "Male";
+const height = "5'10\"";
+let favoriteFood = "Hot wings";
